510f99506dbec324ec24d601eac2b676ec259bbc
LLQualims <luka.lai@qualims.com>
2024-07-04 12:19:47 +0200

    Ajoutez des fichiers projet.

---
+++ AndroidAPPS/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AndroidAPPS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
